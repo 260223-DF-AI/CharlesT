2918635ae0a931582047db16d996fc7f55871f59
Tenuuu <charlestenuta@gmail.com>
2026-03-03 15:25:26 -0600

    progress on file_reader.py

--- FileProcessing/exceptions.py
+++ FileProcessing/exceptions.py
@@ -4,8 +4,16 @@ class FileProcessingError(Exception):
 
 class InvalidDataError(FileProcessingError):
     """Raised when data validation fails."""
-    pass
+
+    # Currently a placeholder
+    def __init__(self, data):
+        self.data = data
+        super().__init__(f"Invalid data: {self.data}")
 
 class MissingFieldError(FileProcessingError):
     """Raised when a required field is missing."""
-    pass
+
+    # Currently a placeholder
+    def __init__(self, field):
+        self.field = field
+        super().__init__(f"Missing required field: {self.field}")
--- FileProcessing/file_reader.py
+++ FileProcessing/file_reader.py
@@ -1,3 +1,4 @@
+records = []
 def read_csv_file(filepath):
     """
     Read a CSV file and return a list of dictionaries.
@@ -10,4 +11,33 @@ def read_csv_file(filepath):
     Returns: List of dictionaries (one per row)
     Raises: FileProcessingError with descriptive message
     """
-    pass
+    
+    with open(filepath, "r") as f:
+        for i, line in enumerate(f):
+            if i == 0:
+                column_names = line.split(",")
+            else:
+                entry = line.split(",")[0:5]
+                records.append({
+                    column_names[0]: entry[0],
+                    column_names[1]: entry[1],
+                    column_names[2]: entry[2],
+                    column_names[3]: entry[3],
+                    column_names[4]: entry[4],
+                })
+
+    print(records)
+
+def save_report(data, filename):
+    with open(filename, "w") as f:
+        f.write(f"Testing\n\n")
+        for record in data:
+            f.write(f"Date: {record['date']}\n")
+            f.write(f"Store ID: {record['store_id']}\n")
+            f.write(f"Product: {record['product']}\n")
+            f.write(f"Quantity: {record['quantity']}\n")
+            f.write(f"Price: {record['price\n']}\n\n")
+            f.write(f"-"*20 + "\n")
+
+read_csv_file("starter_code/sample_sales.csv")
+save_report(records, "test_report.txt")
